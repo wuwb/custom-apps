7a6b6faa3463ac4728b667be71ea2044497f9367
Wu Wenbin <541330190@qq.com>
2023-03-12 11:40:41 +0800

    feat: add media applications

---
+++ engineering/qinglong/bash.sh
@@ -0,0 +1,5 @@
+docker exec -it qinglong bash
+# ql repo https://github.com/zero205/JD_tencent_scf.git "jd_|jx_|getJDCookie" "backUp|icon" "^jd[^_]|USER|sendNotify|sign_graphics_validate|JDJR|JDSign" "main"
+
+# https://github.com/zero205/JD_tencent_scf/blob/main/backUp/GetJdCookie2.md
+
